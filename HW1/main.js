6d31835415c9a1d413e7ef4528185d81d1012e47
MeMrik03 <vip222004@gmail.com>
2023-08-16 14:32:19 +0300

    init

--- HW1/main.js
+++ HW1/main.js
@@ -1,1 +1,26 @@
-console.log()
+let a1 = 'hello'
+let a2 = 'owu'
+let a3 ='com'
+let a4 ='ua'
+let b1 = 1
+let b2 = 10
+let b3 = -999
+let b4 = 123
+let b5 = 3.14
+let b6 = 2.7
+let b7 = 16
+let c1  ='true'
+let c2  ='false'
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(b1)
+console.log(b2)
+console.log(b3)
+console.log(b4)
+console.log(b5)
+console.log(b6)
+console.log(b7)
+console.log(c1)
+console.log(c2)
